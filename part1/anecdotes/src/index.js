3c3ecdc851851f8080b4e257b2bd45aaeb1f2531
morawer <morala84@gmail.com>
2023-07-24 19:45:20 +0200

    Exercise 1.13*

--- part1/anecdotes/src/index.js
+++ part1/anecdotes/src/index.js
@@ -1,27 +1,6 @@
 import React, { useState } from "react";
 import ReactDOM from "react-dom";
 
-const getRandomInt = (props) => { 
-  return Math.floor(Math.random() * props);
-}
-
-const App = (props) => {
-  const [selected, setSelected] = useState(0);
-
-  return (
-		<div>
-			<p>{props.anecdotes[selected]}</p>
-			<button
-				onClick={() =>
-					setSelected(() => getRandomInt(props.anecdotes.length))
-				}
-			>
-				next anecdote
-			</button>
-		</div>
-  );
-};
-
 const anecdotes = [
 	"If it hurts, do it more often",
 	"Adding manpower to a late software project makes it later!",
@@ -29,6 +8,50 @@ const anecdotes = [
 	"Any fool can write code that a computer can understand. Good programmers write code that humans can understand.",
 	"Premature optimization is the root of all evil.",
 	"Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.",
+	"Holaaaa Dani!"
 ];
+
+let anecdotePosition = 0;
+
+const votes = Array(anecdotes.length).fill(0);
+const votesCopy = [...votes]
+
+
+const getRandomInt = (props) => { 
+	anecdotePosition = Math.floor(Math.random() * props);
+	return anecdotePosition;
+}
+
+
+const App = (props) => {
+	const [selected, setSelected] = useState(0);
+	const [votes, setVote] = useState([])
+	console.log(votesCopy);
+	
+	const addVote = (vote) => { 
+		setVote(...votesCopy, vote)
+	}
+
+  return (
+		<div>
+			<p>{props.anecdotes[selected]}</p>
+			<button
+				onClick={() =>
+				  addVote(votesCopy[anecdotePosition]+= 1)
+				}
+			>
+				vote
+			</button>
+			<button
+				onClick={() =>
+					setSelected(() => getRandomInt(props.anecdotes.length))
+				}
+			>
+				next anecdote
+		  </button>
+		</div>
+  );
+};
+
 
 ReactDOM.render(<App anecdotes={anecdotes} />, document.getElementById("root"));
